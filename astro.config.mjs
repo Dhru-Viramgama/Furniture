0cb40f6591fb5a9c427fb4bf6762ae21d9e52287
Dhru Viramgama <dhru@linearloop.io>
2024-09-23 17:20:57 +0530

    fix build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  adapter: vercel()
 });
